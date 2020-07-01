a4dd79bc1e7e8d25303c1aed1a209e6bbfc8e8b7
Ryan <46852695+ryanAllMad@users.noreply.github.com>
2020-07-01 09:47:03 -0700

    Ad tester script file added to repo

---
+++ Ad-Tester.js
@@ -0,0 +1,49 @@
+
+/***
+ * PLACE 'ADS' IN SIDEBAR AND TEST COMSCORE
+ */
+/**
+*insert selectors and positions for sidebar ATF, BTF, and ComScore
+
+Positions: beforebegin, afterbegin, beforeend, afterend
+**/
+var atf = document.querySelector('/**insert selector**/');
+var atfPos = '/**insert position**/';
+var btf = document.querySelector('/**insert selector**/');
+var btfPos = '/**insert position**/';
+var  comscore = document.querySelector('/**insert selector**/');
+
+/**
+*
+**/
+/**
+*
+**/
+var comscorePos = 'beforeend';
+
+var ad = `<p style="background:gray; width:300px; height:300px;"></p>`;
+  var comAd = `<div id="footer_branding" style="text-align : center; font-size : 11px; margin-top : 10px;">
+    Exclusive Member of Mediavine Home
+  </div>`;
+/**
+*script below will place the html fake ads
+**/
+
+atf.insertAdjacentHTML(atfPos, ad);
+btf.insertAdjacentHTML(btfPos, ad);
+comscore.insertAdjacentHTML(comscorePos, comAd);
+
+/**
+*ComScore ONLY
+**/
+
+var  comscore = document.querySelector('/**insert selector**/');
+
+/**
+*
+**/
+var comscorePos = 'beforeend';
+  var comAd = `<div id="footer_branding" style="text-align : center; font-size : 11px; margin-top : 10px;">
+    Exclusive Member of Mediavine Home
+  </div>`;
+  comscore.insertAdjacentHTML(comscorePos, comAd);
